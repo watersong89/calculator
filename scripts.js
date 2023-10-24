161032736dd2a9247273647e4c761831f3c7f8f3
Watersong89 <watersongordon@gmail.com>
2023-10-24 15:31:13 +0100

    simplified operate function

--- scripts.js
+++ scripts.js
@@ -10,30 +10,14 @@ let operatorButtons = document.querySelectorAll('.js-operator-button');
 let equalsButton = document.querySelector('.js-equals-button');
 let clearButton = document.querySelector('.js-clear-button');
 
-function add(num1, num2) {
-  return Number(num1) + Number(num2);
-}
-
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
 function operate(num1, operator, num2) {
   if (operator === '+') {
-    return add(num1, num2);
+    return Number(num1) + Number(num2);
   } if (operator === '-') {
-    return subtract(num1, num2);
+    return Number(num1) - Number(num2);
   } if (operator === '*') {
-    return multiply(num1, num2);
+    return Number(num1) * Number(num2);
   } if (operator === '/') {
-    return divide(num1, num2);
+    return Number(num1) / Number(num2);
   }
 }
