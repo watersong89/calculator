ab42a34b31452f30898d1bf03da4e4b2a5f39e72
Watersong89 <watersongordon@gmail.com>
2023-10-24 14:34:43 +0100

    fns and e-l's added for basic functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
-let num1;
-let num2;
-let operator;
+let num1 = null;
+let num2 = null;
+let operator = null;
+let displayValue = '';
+
+let displayElement = document.querySelector('.display');
+let numButtons = document.querySelectorAll('.js-number-button');
+let operatorButtons = document.querySelectorAll('.js-operator-button');
+let equalsButton = document.querySelector('.js-equals-button');
 
 function add(num1, num2) {
-  return num1 + num2;
+  return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
@@ -29,3 +35,29 @@ function operate(num1, operator, num2) {
     return divide(num1, num2);
   }
 }
+
+function updateDisplay() {
+  displayElement.textContent = displayValue;
+}
+
+numButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    displayValue += button.value;
+    updateDisplay();
+  })
+})
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    displayValue += ` ${button.value} `
+    updateDisplay();
+  })
+})
+
+equalsButton.addEventListener('click', () => {
+  let operationArray = displayValue.split(" ")
+  num1 = operationArray[0];
+  operator = operationArray[1];
+  num2 = operationArray[2];
+  console.log(operate(num1, operator, num2))
+})
